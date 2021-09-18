7edd53e3ebc19e44d0a0e094c03ea9a9b508f415
KygekDev <68368066+KygekDev@users.noreply.github.com>
2021-09-18 20:38:49 +0800

    start.cmd: No longer using mintty.exe if exists
    In these days, it is very unlikely that some PocketMine-MP users are still using mintty.exe, as it's no longer included inside newer PocketMine-MP PHP binaries. Using older PocketMine-MP PHP binaries which contains mintty.exe are no longer supported by PocketMine-MP. If some users somehow still using mintty.exe, they might manually copied or moved their mintty.exe to inside the bin directory.
    
    In conclusion, I think mintty.exe was used back in the Shoghi days.

--- start.cmd
+++ start.cmd
@@ -18,9 +18,5 @@ if exist PocketMine-MP.phar (
 	exit 1
 )
 
-if exist bin\mintty.exe (
-	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-) else (
-	REM pause on exitcode != 0 so the user can see what went wrong
-	%PHP_BINARY% %POCKETMINE_FILE% %* || pause
-)
+REM pause on exitcode != 0 so the user can see what went wrong
+%PHP_BINARY% %POCKETMINE_FILE% %* || pause
